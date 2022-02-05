a3637257427e522522d39bc1916ed629943617ad
yaroslavtk <98087088+yaroslavtk@users.noreply.github.com>
2022-02-05 21:02:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-$('.multiple-items').slick({
-	infinite: true,
+$('.center').slick({
+	centerMode: true,
+	centerPadding: '60px',
 	slidesToShow: 3,
-	slidesToScroll: 3
+	responsive: [
+	  {
+		breakpoint: 768,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 3
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 1
+		}
+	  }
+	]
   });
