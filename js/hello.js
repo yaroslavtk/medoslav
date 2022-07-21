ff4b3585c0e9df85aa9947305049f6da1a152d79
yaroslavtk <98087088+yaroslavtk@users.noreply.github.com>
2022-07-21 08:23:19 +0300

    Add files via upload

--- js/hello.js
+++ js/hello.js
@@ -1,5 +1,5 @@
-let hello_1 = document.querySelector('.hello-1');
-let hello_2 = document.querySelector('.hello-2');
+let wellcome_1 = document.querySelector('.wellcome-text-1');
+let wellcome_2 = document.querySelector('.wellcome-text-2');
 
 // Добавить наблюдение за появлением элемента
 //let hello_observer = new IntersectionObserver(entries => {
@@ -15,7 +15,7 @@ let hello_2 = document.querySelector('.hello-2');
 
 window.onscroll = function () {
   if (window.pageYOffset > 100) {
-    hello_1.classList.add('hello-1-active');
-    hello_2.classList.add('hello-2-active');
+    wellcome_1.classList.add('wellcome-text-1-active');
+    wellcome_2.classList.add('wellcome-text-2-active');
     } 
 };
