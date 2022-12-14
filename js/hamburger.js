3ba543c17cc792ddc15f8ddf65480f0e6d19c807
yaroslavtk <98087088+yaroslavtk@users.noreply.github.com>
2022-12-14 11:15:19 +0200

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -12,24 +12,6 @@ window.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('top-navigation-active');
-        })
-    })
-})
-
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+        });
     });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+});
