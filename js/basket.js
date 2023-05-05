42a2877d207a68cca5a8b3d38a3bd9702dc880fc
Yaroslav <yaroslav.11.tk@gmail.com>
2023-05-05 07:25:57 +0300

    top btns, modal fix

--- js/basket.js
+++ js/basket.js
@@ -1,13 +1,12 @@
 let bin_btns = document.querySelectorAll('.add-to-cart')
 let products_number = document.querySelector('.producs-added')
-let top_bin = document.querySelector('.top-bin')
 let counter = 0;
 
 for (let bin_btn of bin_btns) {
     bin_btn.onclick = function () {
     counter++;
     products_number.textContent = counter;   
-    top_bin.classList.add('product_inside')
+    products_number.classList.add('product_inside')
 }
 };
 
